9e5b607f658b01a69d45eb1ed9abb4736f7f6862
0xSchnappi <952768182@qq.com>
2024-07-21 19:42:38 +0800

    创建异步

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
  * @Author: 0xSchnappi 952768182@qq.com
  * @Date: 2024-07-21 16:58:33
  * @LastEditors: 0xSchnappi 952768182@qq.com
- * @LastEditTime: 2024-07-21 17:19:50
+ * @LastEditTime: 2024-07-21 19:40:00
  * @FilePath: /mini-redis/src/main.rs
  * @Description: redis服务端
  *
@@ -19,20 +19,57 @@ async fn main() {
     loop {
         // 第二个被忽略的项中包含有新连接的`IP`和端口信息
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+        // 为每一条连接都生成一个新任务
+        // `socket`的所有权将被移动到新的任务中，并在那里进行处理
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
+
+    // let handle = tokio::spawn(async { 10086 });
+    // let out = handle.await.unwrap();
+    // println!("GOT {}", out);
 }
 
 async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+    // 使用 hashmap 来存储 redis 的数据
+    let mut db = HashMap::new();
+
     // `Connection` 对于 redis 的读写进行了抽象封装，因此我们读到的是一个一个数据帧frame(数据帧 = redis命令 + 数据)，而不是字节流
     // `Connection` 是在mini-redis中定义
+    // `mini-redis` 提供的遍历函数，使用返回的`connection` 可以用于从socket中读取并解析为数据帧
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    // 使用`read_frame`方法从连接获取一个数据帧： 一条redis命令 + 相应的数据
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                // 值被存储为 `Vec<u8>` 的形式
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    // `Frame::Bulk` 期待数据的类型是 `Bytes`，
+                    // 此时，你只要知道`&Vec<u8>`可以使用`into()`方法转换成`Bytes`类型
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("Unimplemented {:?}", cmd),
+        };
 
-        // 回复一个错误
-        let response = Frame::Error("Unimplemented".to_string());
+        // 将请求响应返回给客户端
         connection.write_frame(&response).await.unwrap();
     }
+    // if let Some(frame) = connection.read_frame().await.unwrap() {
+    //     println!("GOT: {:?}", frame);
+
+    //     // 回复一个错误
+    //     let response = Frame::Error("Unimplemented".to_string());
+    //     connection.write_frame(&response).await.unwrap();
+    // }
 }
